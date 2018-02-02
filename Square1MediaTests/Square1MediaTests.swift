7c54ff03f1c8426a2cd8e35355b954420f4b1b02
Roberto Pastor <wedge.hero@gmail.com>
2018-02-02 15:42:18 +0100

    Initial Commit

---
+++ Square1MediaTests/Square1MediaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Square1MediaTests.swift
+//  Square1MediaTests
+//
+//  Created by Roberto Pastor Ortiz on 2/2/18.
+//  Copyright © 2018 Square1. All rights reserved.
+//
+
+import XCTest
+@testable import Square1Media
+
+class Square1MediaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
